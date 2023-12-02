4f75abf159a1e61759586996d92d1def010d752e
iagoxz <iagomoura640@gmail.com>
2023-12-02 18:55:18 -0300

    update_cardPost

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,104 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import axios from 'axios';
 
-const Cards = () => {
+const CardContainer = styled('div')({
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'center',
+});
+
+const CustomCard = styled(Card)({
+  width: '300px', // Set your desired width
+  margin: '10px', // Adjust margin as needed
+  '@media (max-width: 600px)': {
+    width: '100%', // Make cards full width on smaller screens
+  },
+});
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function RecipeReviewCard() {
+  const [infoCards, setInfoCards] = useState([]);
+  const [expanded, setExpanded] = useState({});
+
+  const handleExpandClick = (id) => {
+    setExpanded((prevExpanded) => ({
+      ...prevExpanded,
+      [id]: !prevExpanded[id],
+    }));
+  };
+
+  async function CardInfo() {
+    try {
+      const { data } = await axios.get("http://localhost:3000/post/listar");
+      setInfoCards(data);
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching profile:', error);
+    }
+  }
+
+  useEffect(() => {
+    CardInfo();
+  }, []);
+
   return (
-    <div>
-      card
-    </div>
-  )
+    <CardContainer>
+      {infoCards.map((infoCard) => (
+        <CustomCard key={infoCard.id}>
+          <CardHeader title={infoCard.user} />
+          <a href={infoCard.productLink} target="_blank" rel="noopener noreferrer">
+            <img src={infoCard.productLink}  alt="link da Promoçâo" />
+          </a>
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              {infoCard.productName}
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon />
+            </IconButton>
+            <ExpandMore
+              expand={expanded[infoCard.id]}
+              onClick={() => handleExpandClick(infoCard.id)}
+              aria-expanded={expanded[infoCard._v]}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
+          </CardActions>
+          <Collapse in={expanded[infoCard.id]} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph>Method: {infoCard.productDescription}</Typography>
+            </CardContent>
+          </Collapse>
+        </CustomCard>
+      ))}
+    </CardContainer>
+  );
 }
-
-export default Cards
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,31 +1,12 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Navbar from '../components/NavBar';
+import RecipeReviewCard from '../components/Cards';
 const Inicio = () => {
-  const [useState, setUseState] = React.useState([]);
-
-  async function getPosts() {
-    const response = await fetch('https://localhost:5000/posts');
-    const data = await response.json();
-    setUseState(data);
-  }
-
-  useEffect(() => {
-    getPosts();
-  }
-    , []);
-
   return (
     <div>
      <Navbar /> 
-      <button onClick={getPosts}>Get Posts</button>
-      {useState.map((post) => (
-        <div key={post.id}>
-          <h3>{post.title}</h3>
-          <h3>{post.description}</h3>
-          <p>{post.body}</p>
-        </div>
-      ))}
-      <h1>Inicio page</h1>
+     <RecipeReviewCard/>
+     
     </div>
   );
 };
