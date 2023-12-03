02000b80bc6c2beb9978251024a30047129dc14a
iagoxz <iagomoura640@gmail.com>
2023-12-03 15:28:52 -0300

    menu hamb add

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -8,8 +8,8 @@ import InputBase from '@mui/material/InputBase';
 import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-import { IoMdMenu } from "react-icons/io";
-import imagem from "../img/logo_1.png";
+import { IoMdMenu } from 'react-icons/io';
+import imagem from '../img/logo_1.png';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -19,7 +19,7 @@ const Search = styled('div')(({ theme }) => ({
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginRight: theme.spacing(2),
-  marginLeft: 0,
+  marginLeft: 'auto',
   width: '100%',
   [theme.breakpoints.up('sm')]: {
     marginLeft: theme.spacing(3),
@@ -91,8 +91,8 @@ export default function PrimarySearchAppBar() {
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Perfil</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Sair</MenuItem>
     </Menu>
   );
 
@@ -115,9 +115,9 @@ export default function PrimarySearchAppBar() {
     >
       <MenuItem>
         <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="error"></Badge>
+          <Badge badgeContent={0} color="error"></Badge>
         </IconButton>
-        <p>Messages</p>
+        <p>Favoritos</p>
       </MenuItem>
       <MenuItem>
         <IconButton
@@ -125,19 +125,19 @@ export default function PrimarySearchAppBar() {
           aria-label="show 17 new notifications"
           color="inherit"
         >
-          <Badge badgeContent={17} color="error"></Badge>
+          <Badge badgeContent={0} color="error"></Badge>
         </IconButton>
-        <p>Notifications</p>
+        <p>Posts</p>
       </MenuItem>
       <MenuItem onClick={handleProfileMenuOpen}>
         <IconButton
           size="large"
           aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
+          aria-controls={menuId}
           aria-haspopup="true"
           color="inherit"
         ></IconButton>
-        <p>Profile</p>
+        <p>Perfil</p>
       </MenuItem>
     </Menu>
   );
@@ -151,18 +151,22 @@ export default function PrimarySearchAppBar() {
             edge="start"
             color="inherit"
             aria-label="open drawer"
-            sx={{ mr: 70}}
+            sx={{ mr: 2 }}
+            onClick={handleMobileMenuOpen}
           >
             <IoMdMenu />
           </IconButton>
-          <Search>
-            <SearchIconWrapper></SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
+            <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+              <Search>
+                <SearchIconWrapper></SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Pesquisar…"
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </Search>
+            </Box>
+          </Box>
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
             <IconButton size="large" aria-label="show 4 new mails" color="inherit">
               <Badge badgeContent={421} color="error"></Badge>
@@ -185,7 +189,6 @@ export default function PrimarySearchAppBar() {
             ></IconButton>
           </Box>
           <img src={imagem} style={{ width: 80 }} />
-
           <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
             <IconButton
               size="large"
